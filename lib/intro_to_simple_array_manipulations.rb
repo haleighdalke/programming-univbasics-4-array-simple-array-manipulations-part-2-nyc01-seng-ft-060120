9925c3cd2b8a8bcc672389bed7950a973c7d35f6
Haleigh Dalke <haleighdalke@gmail.com>
2020-05-08 19:39:48 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,15 @@
+def using_concat(array1, array2)
+  array1.concat(array2)
+end
+
+def using_insert(array, element)
+  array[4] = element
+end
+
+using_uniq
+
+using_flatten
+
+using_delete
+
+using_delete_at
